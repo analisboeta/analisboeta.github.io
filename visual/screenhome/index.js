e08632f617c907372efeb0b5dbd3ba7960d495a5
(Ext.) Ana Lourenço <Lourenco.An@stef-tfe.nt>
2019-10-09 09:52:54 +0100

    started galery for instagram images

--- visual/screenhome/index.js
+++ visual/screenhome/index.js
@@ -1,9 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
- M.Parallax.init(document.querySelectorAll('.parallax'));
- M.Pushpin.init(document.querySelectorAll('.pushpin'));
+document.addEventListener('DOMContentLoaded', function () {
+  M.Parallax.init(document.querySelectorAll('.parallax'));
+  M.Pushpin.init(document.querySelectorAll('.pushpin'));
 });
 
-$('.pushpin-demo-nav').each(function() {
+$('.pushpin-demo-nav').each(function () {
   var $this = $(this);
   var $target = $('#' + $(this).attr('data-target'));
   $this.pushpin({
@@ -11,3 +11,59 @@ $('.pushpin-demo-nav').each(function() {
     bottom: $target.offset().top + $target.outerHeight() - $this.height()
   });
 });
+
+
+axios.get('https://www.instagram.com/explore/tags/analisboetasitegallery/?__a=1')
+  .then(function (response) {
+
+    return response.data.graphql.hashtag.edge_hashtag_to_media.edges;
+  }).then(function (postsArray) {
+
+    let itemArray = [];
+    postsArray.forEach(element => {
+      itemArray.push({
+        imageURL: element.node.display_url,
+        commentsURL: getComment(element.node)
+
+      })
+    });
+    return itemArray;
+  }).then(function (itemArray) {
+
+    const row = document.querySelector('#gallery')
+
+    row.innerHTML ="";
+    itemArray.forEach(element => {
+      row.innerHTML+=cardGenerator(element);
+    });
+  })
+
+  function getComment(node){
+      if(node.edge_media_to_caption.edges){
+        return node.edge_media_to_caption.edges[0].node.text
+      }else {
+        return ""
+      }
+  }
+
+function cardGenerator(item) {
+
+  // <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+  const cardHtml = `
+  <div class="col s12 m6 l4">
+    <div class="card medium">
+      <div class="card-image waves-effect waves-block waves-light">
+        <img class="activator" src="${item.imageURL}">
+      </div>
+      <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4">${item.commentsURL}   <i class="material-icons right">close</i></span>
+      </div>
+    </div>
+  </div>
+  `
+  return cardHtml;
+}
+
+
+
+
